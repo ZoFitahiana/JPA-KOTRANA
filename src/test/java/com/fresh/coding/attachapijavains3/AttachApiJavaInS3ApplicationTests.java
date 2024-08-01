abe8eabc7de4e96e304f55fa0b4d347da430f159
Hevitriniavo <hei.tantely@gmail.com>
2024-08-01 11:31:42 +0300

    first commit

---
+++ src/test/java/com/fresh/coding/attachapijavains3/AttachApiJavaInS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresh.coding.attachapijavains3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachApiJavaInS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
